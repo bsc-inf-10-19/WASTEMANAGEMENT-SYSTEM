3e9144d11c98b02d050324edbc5d29d2ae01781e
MPHATSO MATEMBA <BSC/COM/NE/04/19@COMLAB.COM>
2024-07-13 18:21:41 +0200

    added login page(no backend) and smart waste home screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,9 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10") // Fixed dependency for navigation-compose
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
+    implementation("androidx.compose.ui:ui:1.1.0-beta01")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha03")
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -66,5 +68,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-
 }
--- app/src/main/java/com/example/myapplicationwmsystem/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationwmsystem/MainActivity.kt
@@ -8,16 +8,25 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import androidx.compose.foundation.clickable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
@@ -85,10 +94,19 @@ fun AppNavigation() {
             SplashScreen(onTimeout = { navController.navigate("login_screen") })
         }
         composable("login_screen") {
-            LoginScreen(onLoginSuccess = { navController.navigate("garbage_level_screen") })
+            LoginScreen(onLoginSuccess = { navController.navigate("home_screen") })
+        }
+        composable("home_screen") {
+            HomeScreen(navController)
         }
         composable("garbage_level_screen") {
             GarbageLevelScreen()
+        }
+        composable("analytics_screen") {
+            AnalyticsScreen()
+        }
+        composable("bin_location_screen") {
+            BinLocationScreen()
         }
     }
 }
@@ -117,7 +135,9 @@ fun SplashScreen(onTimeout: () -> Unit) {
 
 @Composable
 fun LoginScreen(onLoginSuccess: () -> Unit) {
-    // Simulate a login process
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
@@ -125,15 +145,96 @@ fun LoginScreen(onLoginSuccess: () -> Unit) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Login", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            Image(
+                painter = painterResource(id = R.drawable.ic_profile),
+                contentDescription = "Profile Image",
+                modifier = Modifier
+                    .size(100.dp)
+                    .background(MaterialTheme.colorScheme.primary, shape = CircleShape)
+                    .padding(16.dp)
+            )
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { onLoginSuccess() }) {
-                Text(text = "Login")
+            OutlinedTextField(
+                value = username,
+                onValueChange = { username = it },
+                label = { Text("Username") },
+                modifier = Modifier.fillMaxWidth(0.8f)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Password") },
+                modifier = Modifier.fillMaxWidth(0.8f),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Button(onClick = { onLoginSuccess() }) {
+                    Text("Sign In")
+                }
+                Button(onClick = { /* Handle sign up */ }) {
+                    Text("Sign Up")
+                }
             }
         }
     }
 }
 
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "smart waste management",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(8.dp)
+        )
+        HomeScreenItem(
+            imageRes = R.drawable.bin_profile,
+            text = "Bin Level",
+            onClick = { navController.navigate("garbage_level_screen") }
+        )
+        HomeScreenItem(
+            imageRes = R.drawable.analytics_profile,
+            text = "Analytics",
+            onClick = { navController.navigate("analytics_screen") }
+        )
+        HomeScreenItem(
+            imageRes = R.drawable.location_profile,
+            text = "Bin Location",
+            onClick = { navController.navigate("bin_location_screen") }
+        )
+    }
+}
+
+@Composable
+fun HomeScreenItem(imageRes: Int, text: String, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.secondary, shape = CircleShape)
+            .clickable { onClick() }
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = null,
+            modifier = Modifier.size(48.dp)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = text, fontSize = 18.sp, fontWeight = FontWeight.Medium)
+    }
+}
 @Composable
 fun GarbageLevelScreen() {
     var garbageLevel by remember { mutableStateOf(0) }
@@ -141,14 +242,18 @@ fun GarbageLevelScreen() {
     val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) {
-        while (true) {
-            coroutineScope.launch {
-                garbageLevel = fetchGarbageLevelFromThingSpeak()
-                if (garbageLevel > 70) {
-                    showNotification(context, garbageLevel)
+        coroutineScope.launch {
+            while (true) {
+                try {
+                    garbageLevel = fetchGarbageLevelFromThingSpeak()
+                    if (garbageLevel > 80) {
+                        showNotification(context, garbageLevel)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace() // Log the exception
                 }
+                delay(3000) // Update every 3 seconds
             }
-            delay(3000) // Update every 3 seconds
         }
     }
 
@@ -188,65 +293,67 @@ fun GarbageLevelScreen() {
     }
 }
 
-private suspend fun fetchGarbageLevelFromThingSpeak(): Int {
-    return withContext(Dispatchers.IO) {
-        try {
-            val apiKey = "H8M68IKI5A3QYVET"
-            val channelId = "2595920"
-            val url = URL("https://api.thingspeak.com/channels/$channelId/fields/1.json?api_key=$apiKey&results=1")
 
-            with(url.openConnection() as HttpURLConnection) {
-                requestMethod = "GET"
-                inputStream.bufferedReader().use {
-                    val response = it.readText()
-                    val json = JSONObject(response)
-                    val feeds = json.getJSONArray("feeds")
-                    if (feeds.length() > 0) {
-                        val lastEntry = feeds.getJSONObject(0)
-                        return@withContext lastEntry.getInt("field1")
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return@withContext 0
+@Composable
+fun AnalyticsScreen() {
+    // Implementation for Analytics Screen
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Analytics Screen", fontSize = 24.sp, fontWeight = FontWeight.Bold)
     }
 }
 
-private fun showNotification(context: Context, garbageLevel: Int) {
-    val builder = NotificationCompat.Builder(context, "GARBAGE_ALERT_CHANNEL")
-        .setSmallIcon(android.R.drawable.ic_dialog_alert)
-        .setContentTitle("Garbage Level Alert")
-        .setContentText("Garbage level is at $garbageLevel% full. Please consider emptying it soon.")
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
-        .setDefaults(NotificationCompat.DEFAULT_ALL) // Ensure the notification pops up
+@Composable
+fun BinLocationScreen() {
+    // Implementation for Bin Location Screen
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Bin Location Screen", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+    }
+}
 
+fun showNotification(context: Context, level: Int) {
+    val builder = NotificationCompat.Builder(context, "GARBAGE_ALERT_CHANNEL")
+        .setSmallIcon(R.drawable.ic_notification)
+        .setContentTitle("Garbage Level Alert")
+        .setContentText("Garbage level is at $level%")
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
     with(NotificationManagerCompat.from(context)) {
         notify(1, builder.build())
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun SplashScreenPreview() {
-    MyApplicationWMsystemTheme {
-        SplashScreen(onTimeout = {})
+suspend fun fetchGarbageLevelFromThingSpeak(): Int {
+    val apiKey = "H8M68IKI5A3QYVET"
+    val channelId = "2595920"
+    val url = URL("https://api.thingspeak.com/channels/$channelId/feeds.json?api_key=$apiKey&results=1")
+    return withContext(Dispatchers.IO) {
+        (url.openConnection() as? HttpURLConnection)?.run {
+            requestMethod = "GET"
+            inputStream.bufferedReader().use { reader ->
+                val response = reader.readText()
+                val json = JSONObject(response)
+                val feeds = json.getJSONArray("feeds")
+                if (feeds.length() > 0) {
+                    val lastEntry = feeds.getJSONObject(0)
+                    val field1 = lastEntry.optString("field1")
+                    field1.toIntOrNull() ?: 0
+                } else {
+                    0
+                }
+            }
+        } ?: 0
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun LoginScreenPreview() {
+fun DefaultPreview() {
     MyApplicationWMsystemTheme {
-        LoginScreen(onLoginSuccess = {})
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GarbageLevelScreenPreview() {
-    MyApplicationWMsystemTheme {
-        GarbageLevelScreen()
+        AppNavigation()
     }
 }
