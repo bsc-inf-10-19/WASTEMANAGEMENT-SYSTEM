eb4b2d9fe7e160f382b8aa2267356ae9018ce6f8
MPHATSO MATEMBA <BSC/COM/NE/04/19@COMLAB.COM>
2024-07-15 15:57:16 +0200

    modified home screen with better HCI

--- app/src/main/java/com/example/myapplicationwmsystem/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationwmsystem/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -262,21 +263,29 @@ fun SignUpScreen(onSignUpSuccess: () -> Unit) {
         }
     }
 }
-
 @Composable
 fun HomeScreen(navController: NavHostController) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(vertical = 16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        Text(
-            text = "Smart Waste Management",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(8.dp)
-        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(14.dp)
+        ) {
+            Text(
+                text = "Smart Waste Management",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White //
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
         HomeScreenItem(
             imageRes = R.drawable.bin_profile,
             text = "Bin Level",
@@ -295,12 +304,13 @@ fun HomeScreen(navController: NavHostController) {
     }
 }
 
+
 @Composable
 fun HomeScreenItem(imageRes: Int, text: String, onClick: () -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .background(MaterialTheme.colorScheme.secondary, shape = CircleShape)
+           // .background(MaterialTheme.colorScheme.secondary, shape = CircleShape)
             .clickable { onClick() }
             .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically
